dd9cf8f76aba2f94fc94d6f2206381b284c0a698
shrr98 <shinrezky@gmail.com>
2026-09-19 16:47:33 +0700

    fix: gh-pages deploy issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './Home.jsx'
 import About from './About.jsx'
 import Exhibition from './Exhibition.jsx'
